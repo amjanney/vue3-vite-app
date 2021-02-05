08f407fad7bce5eeccb748d687628bc4b426d0e8
朽木白 <1547702880@qq.com>
2021-02-05 11:21:07 +0800

    feat：添加全局样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import element3 from "element3";
 import "element3/lib/theme-chalk/index.css";
 import App from './App.vue'
-import './index.css'
+
+// 全局样式
+import "/@styles/index.scss";
 
 createApp(App).use(element3).mount('#app')
